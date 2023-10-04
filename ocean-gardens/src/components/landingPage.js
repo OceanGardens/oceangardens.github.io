d0455318c45f4ba944111457fd70fccd8b0a4737
Bruno Amaral Teixeira de Freitas <ra246983@lawrence.lab.ic.unicamp.br>
2023-10-04 17:50:50 -0300

    more responsive landing page but without animation

--- ocean-gardens/src/components/landingPage.js
+++ ocean-gardens/src/components/landingPage.js
@@ -16,16 +16,7 @@ function LandingPage() {
             >
                 {({pause, play}) => (
                     <div id="landingPageTextContainer">
-                            {window.innerWidth > 1024 ? (
-                                <div id="animatedTitleContainer">
-                                    <div className='textAlign'>
-                                        <h1>Ocean Gardens</h1>
-                                        <h1>Ocean Gardens</h1>
-                                    </div>
-                                </div>
-                            ): (
-                                <h1>Ocean Gardens</h1>
-                            )}
+                        <h1>Ocean Gardens</h1>
                         <h2>How the oceans play a key role in Earth's biosphere</h2>
                     </div>
                 )}
