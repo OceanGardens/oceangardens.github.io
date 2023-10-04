801cec773233bad98d0b5fb18ec25c32eebb7d8f
Felipe Gabriel Brabes da Silva <ra247085@joyce.lab.ic.unicamp.br>
2023-10-04 17:41:55 -0300

    Adicionando imagens a mais para backgorund

--- ocean-gardens/src/components/landingPage.js
+++ ocean-gardens/src/components/landingPage.js
@@ -1,19 +1,36 @@
 import WaterWave from 'react-water-wave';
-import Image from "../resources/wallpaper_2.jpg"
+import Image1 from "../resources/wallpaper.jpg"
+import Image2 from "../resources/wallpaperMed.jpg"
+import Image1500 from "../resources/wallpaper1500.jpg"
+import ImageSmall from "../resources/imageSmall.jpg"
 import "../styles/landingPage.css"
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function LandingPage() {
 
+    const getimage=(width, height)=>{
+        if(width < 670 && height < 1000){
+            return ImageSmall
+        }else if(width < 1500 && height < 1000){
+            return Image1500
+        }else if(width < 2100 && height < 1400){
+            return Image2
+        }else{
+            return Image1
+        }
+    }
+    
   return (
+        
         <div className="landingPage">
             <WaterWave
-                imageUrl={Image}
+                imageUrl={getimage(window.innerWidth, window.innerHeight)}
                 dropRadius={40}
                 className="waterWaveCanvas"
                 perturbance={0.01}
                 resolution={400}
             >
+                
                 {({pause, play}) => (
                     <div id="landingPageTextContainer">
                         <h1>Ocean Gardens</h1>
@@ -21,7 +38,10 @@ function LandingPage() {
                     </div>
                 )}
             </WaterWave>
+            
+            
         </div>
+
   );
 }
 
