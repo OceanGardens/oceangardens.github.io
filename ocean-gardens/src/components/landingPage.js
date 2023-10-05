1927e1402e544c518d434627117a4520890e991a
unknown <lipebrabes@gmail.com>
2023-10-04 22:49:15 -0300

    add - Imagens de fundo para melhorar resolução
    Imagens de fundo de tamanhos diferentes para melhor resolução em diferentes dispositivos. Como a WaterWave usa URL precisamos de várias imagens.

--- ocean-gardens/src/components/landingPage.js
+++ ocean-gardens/src/components/landingPage.js
@@ -1,18 +1,24 @@
 import WaterWave from 'react-water-wave';
-import Image1 from "../resources/wallpaper.jpg"
-import Image2 from "../resources/wallpaperMed.jpg"
-import Image1500 from "../resources/wallpaper1500.jpg"
-import ImageSmall from "../resources/imageSmall.jpg"
+import Image1 from "../resources/wallpaper/wallpaper.jpg"
+import Image2 from "../resources/wallpaper/wallpaperMed.jpg"
+import Image1800 from "../resources/wallpaper/wallpaper1800.jpg"
+import Image1500 from "../resources/wallpaper/wallpaper1500.jpg"
+import ImageSmall from "../resources/wallpaper/imageSmall.jpg"
+import ImageSmaller from "../resources/wallpaper/imageSmaller.jpg"
 import "../styles/landingPage.css"
 import React, { useEffect, useState } from 'react';
 
 function LandingPage() {
 
-    const getimage=(width, height)=>{
-        if(width < 670 && height < 1000){
+    const getBackgroundImage=(width, height)=>{
+        if(width < 450 && height < 675){
+            return ImageSmaller
+        }else if(width < 670 && height < 1000){
             return ImageSmall
         }else if(width < 1500 && height < 1000){
             return Image1500
+        }else if(width < 1800 && height < 1200){
+            return Image1800
         }else if(width < 2100 && height < 1400){
             return Image2
         }else{
@@ -24,7 +30,7 @@ function LandingPage() {
         
         <div className="landingPage">
             <WaterWave
-                imageUrl={getimage(window.innerWidth, window.innerHeight)}
+                imageUrl={getBackgroundImage(window.innerWidth, window.innerHeight)}
                 dropRadius={40}
                 className="waterWaveCanvas"
                 perturbance={0.01}
