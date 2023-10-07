072f93d7ef18b3e0d7228e70d3840cd1ff7c4c85
Bernardo Panka Archegas <ra246970@souza.lab.ic.unicamp.br>
2023-10-06 21:37:56 -0300

    Integracao Unity-React feita

--- ocean-gardens/src/App.js
+++ ocean-gardens/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React, { useState, useEffect } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
+  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
+    loaderUrl: "build/OceanGardener.loader.js",
+    dataUrl: "build/OceanGardener.data",
+    frameworkUrl: "build/OceanGardener.framework.js",
+    codeUrl: "build/OceanGardener.wasm",
+  });
+
+  // We'll use a state to store the device pixel ratio.
+  const [devicePixelRatio, setDevicePixelRatio] = useState(
+    window.devicePixelRatio
+  );
+
+  useEffect(
+    function () {
+      // A function which will update the device pixel ratio of the Unity
+      // Application to match the device pixel ratio of the browser.
+      const updateDevicePixelRatio = function () {
+        setDevicePixelRatio(window.devicePixelRatio);
+      };
+      // A media matcher which watches for changes in the device pixel ratio.
+      const mediaMatcher = window.matchMedia(
+        `screen and (resolution: ${devicePixelRatio}dppx)`
+      );
+      // Adding an event listener to the media matcher which will update the
+      // device pixel ratio of the Unity Application when the device pixel
+      // ratio changes.
+      mediaMatcher.addEventListener("change", updateDevicePixelRatio);
+      return function () {
+        // Removing the event listener when the component unmounts.
+        mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
+      };
+    },
+    [devicePixelRatio]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Unity
+      unityProvider={unityProvider}
+      style={{ width: 1600, height: 900 }}
+      devicePixelRatio={devicePixelRatio}
+    />
   );
 }
 
