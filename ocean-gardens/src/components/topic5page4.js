ca69afc947c1facbfc4c266def13191608b9e0e5
berarchegas <b246970@dac.unicamp.br>
2023-10-08 15:36:05 -0300

    Topicos 5 e 6

---
+++ ocean-gardens/src/components/topic5page4.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import {motion} from 'framer-motion'
+import Image from '../resources/nasa/tasmanSea.jpg'
+import {Link} from "react-router-dom";
+import Button from '@mui/material/Button';
+import {ButtonGroup} from '@mui/material'
+
+function Topic5page4() {
+
+    return(
+
+        <motion.div className = 'topic5page4'
+        initial={{width: 0}}
+        animate={{width: '100%'}}
+        transition={{duration: 0.5}}
+        exit={{x:window.innerWidth}}>
+            <img src = {Image} style={{position:'absolute', top:'0vh', width:'100%', maxHeight:"100vh", zIndex:"-100"}}>     
+            </img>
+            <div style={{position:'absolute', top:'0vh', width:'100%', maxHeight:"100vh"}}>
+                <motion.div
+                initial={{x: '150vw'}}
+                animate={{x: '50vw'}}
+                transition={{duration: 1.2, ease: 'circOut'}}
+                style={{position: 'relative', top:'5vh', left: '0vw', width: '45vw', height:'50vh'}}
+                >
+                    <h1 style={{position:'relative', textAlign: 'justify', color:'white', fontSize: '4.2vh'}}>Lastly, According to the International Panel on Climate Change (IPCC), there is a lot of evidence that human actions are also contributing to an alarming increase in Global Warming.</h1>
+                </motion.div>
+                <motion.div
+                initial={{x: '-105vw'}}
+                animate={{x: '5vw'}}
+                transition={{duration: 1.2, ease: 'circOut', delay:1.5}}
+                style={{position: 'relative', top:'-1vh', width:'45vw'}}
+                >   
+                        <h1 style={{top:'-4vh', position:'relative', color:'#fff', fontSize: '4.2vh', textAlign:'justify'}}>This affects every single aspect of life on Earth, because it drastically changes the way ecosystems interact with each other. The rise in temperature of the Earth is responsible for climate anomalies, melting of polar ice caps, forest fires and the rise of water temperature that impacts all marine life.</h1>
+                        
+                        <div style={{position: 'relative', marginLeft: 'auto', marginRight: '0', bottom: '2vh'}}>
+                            
+                            <ButtonGroup variant="contained" style={{display:'flex', position: 'absolute', top:'-6vh', left: '77vw'}}>
+                                <Link to='../topic5page3'>
+                                    <Button>Previous</Button>
+                                </Link> 
+                                <Link to='../topic6page1'>
+                                    <Button>Next</Button>
+                                </Link>
+                            </ButtonGroup>
+        
+                            
+                        </div>
+        
+                </motion.div>
+            </div>
+        </motion.div>
+        
+    )
+}
+
+export default Topic5page4;   
---
+++ ocean-gardens/src/components/topic6page2.js
@@ -0,0 +1,57 @@
+import React, { useEffect } from 'react';
+import {motion} from 'framer-motion'
+import Image from '../resources/nasa/northwesternIslands.jpg'
+import {Link} from "react-router-dom";
+import Button from '@mui/material/Button';
+import {ButtonGroup} from '@mui/material'
+
+function Topic6page2() {
+
+    return(
+
+        <motion.div className = 'topic6page2'
+        initial={{width: 0}}
+        animate={{width: '100%'}}
+        transition={{duration: 0.5}}
+        exit={{x:window.innerWidth}}>
+            <img src = {Image} style={{position:'absolute', top:'0vh', width:'100%', maxHeight:"100vh", zIndex:"-100"}}>     
+            </img>
+            <div style={{position:'absolute', top:'0vh', width:'100%', maxHeight:"100vh"}}>
+                <motion.div
+                initial={{x: '150vw'}}
+                animate={{x: '50vw'}}
+                transition={{duration: 1.2, ease: 'circOut'}}
+                style={{position: 'relative', top:'5vh', left: '0vw', width: '45vw', height:'50vh'}}
+                >
+                    <h1 style={{position:'relative', color:'#fff', fontSize: '4.5vh', textAlign:'justify'}}> The entire planet has to work together in order to preserve the oceans and our home. Major companies and governments need to create plans of actions to preserve and protect the Aquasphere.</h1>
+                      
+                </motion.div>
+                <motion.div
+                initial={{x: '-105vw'}}
+                animate={{x: '5vw'}}
+                transition={{duration: 1.2, ease: 'circOut', delay:1.5}}
+                style={{position: 'relative', top:'0vh', width:'45vw'}}
+                >   
+                        <h1 style={{position:'relative', color:'#fff', fontSize: '4.5vh', textAlign:'justify'}}>A few key components to achieve this is fighting against illegal mining and fishing, diminishing ocean plastic pollution, reducing gasses emissions and exploring sustainable energy sources.</h1>
+                        <div style={{position: 'relative', marginLeft: 'auto', marginRight: '0', bottom: '-5vh'}}>
+                            
+                            <ButtonGroup variant="contained" style={{display:'flex', position: 'absolute', top:'-10vh', left: '77vw'}}>
+                                <Link to='../topic6page1'>
+                                    <Button>Previous</Button>
+                                </Link> 
+                                <Link to='../topic6page3'>
+                                    <Button>Next</Button>
+                                </Link>
+                            </ButtonGroup>
+        
+                            
+                        </div>
+        
+                </motion.div>
+            </div>
+        </motion.div>
+        
+    )
+}
+
+export default Topic6page2;   
